789ee7e3fa2cfafa57702eac463da89457b47f2f
hossain95 <miahossain8888@gmail.com>
2021-12-14 13:29:48 +0600

    django-rest-api model-update

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from .import views
 urlpatterns = [
     path('categorys/', views.categorys, name="categorys"),
     path('books/', views.books, name="books"),
+    path('book-detail/<int:pk>/', views.bookDetail, name="book-detail"),
+    path('book-update/<int:pk>/', views.bookUpdate, name="book-update"),
+    path('book-delete/<int:pk>/', views.bookDelete, name="book-delete"),
     path('products/', views.products, name="products"),
 ]
