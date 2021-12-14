bda8fa14e121d184e9f4feb67ddd4f6e0d8153ee
hossain95 <miahossain8888@gmail.com>
2021-12-14 12:21:18 +0600

    django-rest-api model-update

---
+++ main/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from main.models import Product, Category, Book
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = (
+            'id',
+            'title'
+        )
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+        # fields = (
+        #     'id',
+        #     'title',
+        #     'isbn',
+        #     'pages',
+        #     'price',
+        #     'stock',
+        #     'descriptions',
+        #     'status',
+        #     'date_created',
+        # )
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'product_tag',
+            'name',
+            'price',
+            'quantity',
+            'descriptions',
+            'stock',
+            'status',
+            'date_created',
+        )
